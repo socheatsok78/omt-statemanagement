1c31f9fa4b92315cf0965e448029692e2dd2f717
Socheat Sok <alex@socheat.net>
2021-07-10 14:54:52 +0700

    feat: install @socheatsok78/sharedworker-loader

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config.module
+      .rule('sharedworker-loader')
+      .test(/\.sharedworker\.(js|ts)$/i)
+        .use('sharedworker-loader')
+        .loader('@socheatsok78/sharedworker-loader')
+        .end()
+  },
+  configureWebpack: {
+    plugins: []
+  }
 }
